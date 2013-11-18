90d56a9eef54ca3cfd3e0a4efd344176bb20d108
imbrianj <bjohnsonwsu@gmail.com>
2013-11-17 20:10:03 -0800

    Now, if switch is on, use the on switch (again) to toggle the switch, leaving the outlet on.  Instructions in comment at top of file for each scenario (as they can be difficult to articulate)

--- switch_controls_outlet.groovy
+++ switch_controls_outlet.groovy
@@ -5,7 +5,17 @@
  *  Date: 2013-11-16
  *
  *  A Z-Wave switch controls any given outlets.  Allows independent control of
- *  outlets by turning the switch off when already in an off state.
+ *  outlets by turning the switch off when already in an off state - or turning
+ *  the switch on when already in an on state.
+ *
+ *  If switch is off: Turn switch on and it will turn on switch and any
+ *  outlets.
+ *  If switch is on: Turn switch off and it will turn off switch and any
+ *  outlets.
+ *  If switch is off: Turn switch off and it will turn on any outlets and
+ *  switch will remain off.
+ *  If switch is on: Turn switch on and it will turn off the switch and any
+ *  outlets will remain on.
  */
 preferences {
   section("Turn on with which switch?") {
@@ -16,8 +26,8 @@ preferences {
     input "outlets", "capability.switch", multiple: true
   }
 
-  section("If switch is off, use off as toggle for outlets?") {
-    input "offToggle", "enum", metadata: [values: ["Yes", "No"]], required: false
+  section("Use switch for additional toggling (on if already on, off if already off)?") {
+    input "toggle", "enum", metadata: [values: ["Yes", "No"]], required: false
   }
 }
 
@@ -31,41 +41,50 @@ def updated() {
 }
 
 def init() {
-  subscribe(wallSwitch, "switch", changeLights)
-
-  if(offToggle == "Yes") {
-    subscribe(wallSwitch, "switch.off", doubleSwitch, [filterEvents: false])
-  }
+  state.wallSwitch = wallSwitch.latestValue("switch")
+  subscribe(wallSwitch, "switch", changeLights, [filterEvents: false])
 }
 
 def changeLights(evt) {
-  if(evt.value == "on") {
-    log.info("Turning on lights")
-
-    outlets?.on()
-  }
-
-  else {
-    log.info("Turning off lights")
-
-    outlets?.off()
-  }
-}
-
-def doubleSwitch(evt) {
   if(evt.isPhysical()) {
-    log.info("Switch is off, but we want to toggle outlets")
-
-    if(outlets.findAll { it?.latestValue("switch") == "on" }) {
-      log.info("Toggle lights off")
-
-      outlets?.off()
+    if((wallSwitch.latestValue("switch") == "on") &&
+       (evt.value == "on") &&
+       (toggle == "Yes") &&
+       (state.wallSwitch == "on")) {
+      log.info("Switch is on, but we want to toggle switch")
+      state.wallSwitch = "off"
+      wallSwitch.off()
     }
 
-    else {
-      log.info("Toggle lights on")
+    else if((wallSwitch.latestValue("switch") == "off") &&
+            (evt.value == "off") &&
+            (toggle == "Yes") &&
+            (state.wallSwitch == "off")) {
+      log.info("Switch is off, but we want to toggle outlets")
+      state.wallSwitch = "off"
+      if(outlets.findAll { it?.latestValue("switch") == "on" }) {
+        log.info("Toggle lights off")
 
+        outlets?.off()
+      }
+
+      else {
+        log.info("Toggle lights on")
+
+        outlets?.on()
+      }
+    }
+      
+    else if(evt.value == "on") {
+      log.info("Turning on lights")
+      state.wallSwitch = "on"
       outlets?.on()
+    }
+
+    else if(evt.value == "off") {
+      log.info("Turning off lights")
+      state.wallSwitch = "off"
+      outlets?.off()
     }
   }
 }
